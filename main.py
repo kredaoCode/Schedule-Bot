0d000e79e0a54998ef98b884faf1dd2173b13db0
Kredao <kredao2007@gmail.com>
2024-08-07 18:31:53 +0600

    create function get_images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 
-token = 'vk1.a.r8ndRoCn4h9RNzgp_qh41hLo6dHfSg0KAJygTtFuZqPF3PyJ5DKykYfvbd8X2r6CrUsWGBrhsrnLLJEIzhHAUSdSS0yX7f7Yzaxgsd-uhCvcndpW9dwNcvHOSPMxCDo9som1fqzMNGhbJiu9cmtRWI6XbTQughjy9RHWAN4RE5zLGz8TGcXoKR6gjZekYeqh7nDMHV8VKp9d7NcI0f2ycA'
+import TOKEN
+
+token = TOKEN.token
 
 
 def get_images():
