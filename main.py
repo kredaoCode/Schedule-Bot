a31d8c235d99a1403bdaac1fdf536f0fc0b89a4c
Kredao <kredao2007@gmail.com>
2024-08-07 18:30:07 +0600

    create function get_images

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import requests
 
 token = 'vk1.a.r8ndRoCn4h9RNzgp_qh41hLo6dHfSg0KAJygTtFuZqPF3PyJ5DKykYfvbd8X2r6CrUsWGBrhsrnLLJEIzhHAUSdSS0yX7f7Yzaxgsd-uhCvcndpW9dwNcvHOSPMxCDo9som1fqzMNGhbJiu9cmtRWI6XbTQughjy9RHWAN4RE5zLGz8TGcXoKR6gjZekYeqh7nDMHV8VKp9d7NcI0f2ycA'
-def get_wall():
+
+
+def get_images():
     response = requests.get(
         'https://api.vk.com/method/wall.get',
         params={
             'access_token': token,
             'domain': 'couples_schedul',
             'v': 5.199,
-            'count': 10
+            'count': 100
         }
     )
-    return response.json()
+    response.json()
 
+    def has_photo(items):
+        return any(item.get("type") == "photo" for item in items)
 
-content = get_wall()
+    content = response.json()['response']['items']
+    content = [item for item in content if
+               "copy_history" not in item and item.get("text") == "" and has_photo(item.get("attachments", []))]
 
-print(content)
+    return [item['photo'] for entry in content for item in entry.get("attachments", [])]
+
+print(get_images())
