0c9d3be38df9e37a87c28c07ba27ba3663ab7376
Kredao <kredao2007@gmail.com>
2024-08-08 23:00:03 +0600

    create bot

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-import requests
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
 
-import TOKEN
+from app.tokens import tg_token
 
-token = TOKEN.token
+bot = Bot(token=tg_token)
+dp = Dispatcher()
 
-
-def get_images():
-    response = requests.get(
-        'https://api.vk.com/method/wall.get',
-        params={
-            'access_token': token,
-            'domain': 'couples_schedul',
-            'v': 5.199,
-            'count': 100
-        }
-    )
-    response.json()
-
-    def has_photo(items):
-        return any(item.get("type") == "photo" for item in items)
-
-    content = response.json()['response']['items']
-    content = [item for item in content if
-               "copy_history" not in item and item.get("text") == "" and has_photo(item.get("attachments", []))]
-
-    return [item['photo'] for entry in content for item in entry.get("attachments", [])]
-
-print(get_images())
+async def main():
+    await dp.start_polling(bot)
+    
+if __name__ == '__main__':
+    asyncio.run(main())
